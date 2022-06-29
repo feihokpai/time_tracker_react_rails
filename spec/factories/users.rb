b56cec0fb7382e1aaa628c9647e6eb286decacca
Fabiano Amorim <fabiano.silva.amorim@gmail.com>
2022-06-28 23:47:58 -0300

    Improve the factories from User and TaskGroup.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :user do
-    
+    email { "doido@gmailll.com" }
+    name { "Pedro Santos" }
+    password { "123456" }
+    admin { false }
+
+    factory :user_admin do
+      admin { true }
+    end
   end
 end
