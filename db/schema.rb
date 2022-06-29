3003e375e03a965437142f73ddfeca7ef26257d9
Fabiano Amorim <fabiano.silva.amorim@gmail.com>
2022-06-28 23:48:28 -0300

    Update schema.rb after creating the table task_groups.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_30_145734) do
+ActiveRecord::Schema.define(version: 2022_06_29_012652) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "task_groups", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.datetime "updated_at", precision: 6, default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.index ["user_id"], name: "index_task_groups_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.boolean "admin", default: false, null: false
@@ -29,4 +37,5 @@ ActiveRecord::Schema.define(version: 2022_01_30_145734) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "task_groups", "users"
 end
