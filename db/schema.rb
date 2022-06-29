c40e49ab4906a117e338f04088e74646edcd14af
Fabiano Amorim <fabiano.silva.amorim@gmail.com>
2022-06-29 12:10:26 -0300

    Update schema.rb.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_29_141811) do
+ActiveRecord::Schema.define(version: 2022_06_29_150042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,15 @@ ActiveRecord::Schema.define(version: 2022_06_29_141811) do
     t.index ["task_group_id"], name: "index_tasks_on_task_group_id"
   end
 
+  create_table "time_registers", force: :cascade do |t|
+    t.bigint "task_id", null: false
+    t.datetime "start_time", default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.datetime "finish_time"
+    t.datetime "created_at", precision: 6, default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.datetime "updated_at", precision: 6, default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.index ["task_id"], name: "index_time_registers_on_task_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.boolean "admin", default: false, null: false
     t.string "name", null: false
@@ -48,4 +57,5 @@ ActiveRecord::Schema.define(version: 2022_06_29_141811) do
 
   add_foreign_key "task_groups", "users"
   add_foreign_key "tasks", "task_groups"
+  add_foreign_key "time_registers", "tasks"
 end
