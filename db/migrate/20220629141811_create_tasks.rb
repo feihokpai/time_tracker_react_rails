2dee88b62acd2f6b26ee547948461a83f4d8363d
Fabiano Amorim <fabiano.silva.amorim@gmail.com>
2022-06-29 11:42:49 -0300

    Change the migration file to turn name NOT NULL.

--- db/migrate/20220629141811_create_tasks.rb
+++ db/migrate/20220629141811_create_tasks.rb
@@ -1,7 +1,7 @@
 class CreateTasks < ActiveRecord::Migration[7.0]
   def change
     create_table :tasks do |t|
-      t.string :name
+      t.string :name, null: false
       t.string :description
       t.references :task_group, null: false, foreign_key: true
 
