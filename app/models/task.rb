2b0dd637adf1e98992db1243c59feadb0807542a
Fabiano Amorim <fabiano.silva.amorim@gmail.com>
2022-06-29 11:54:31 -0300

    Add some mapping attributes among User, TaskGroup and Task.

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,4 @@
 class Task < ApplicationRecord
   belongs_to :task_group
+  delegate :user, to: :task_group
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :task_groups
+  has_many :tasks, through: :task_groups
 end
