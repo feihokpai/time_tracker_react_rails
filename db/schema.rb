c9b65c22473a02c4c628955354b7cbcbd97e602c
Fabiano Amorim <fabiano.silva.amorim@gmail.com>
2023-10-26 12:16:19 -0300

    Run "rails db:migrate" and update schema.rb.

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_29_150042) do
-
+ActiveRecord::Schema[7.1].define(version: 2022_06_29_150042) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "task_groups", force: :cascade do |t|
     t.string "name"
     t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, default: -> { "CURRENT_TIMESTAMP" }, null: false
-    t.datetime "updated_at", precision: 6, default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.datetime "created_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.datetime "updated_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
     t.index ["user_id"], name: "index_task_groups_on_user_id"
   end
 
@@ -27,17 +26,17 @@ ActiveRecord::Schema.define(version: 2022_06_29_150042) do
     t.string "name", null: false
     t.string "description"
     t.bigint "task_group_id", null: false
-    t.datetime "created_at", precision: 6, default: -> { "CURRENT_TIMESTAMP" }, null: false
-    t.datetime "updated_at", precision: 6, default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.datetime "created_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.datetime "updated_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
     t.index ["task_group_id"], name: "index_tasks_on_task_group_id"
   end
 
   create_table "time_registers", force: :cascade do |t|
     t.bigint "task_id", null: false
-    t.datetime "start_time", default: -> { "CURRENT_TIMESTAMP" }, null: false
-    t.datetime "finish_time"
-    t.datetime "created_at", precision: 6, default: -> { "CURRENT_TIMESTAMP" }, null: false
-    t.datetime "updated_at", precision: 6, default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.datetime "start_time", precision: nil, default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.datetime "finish_time", precision: nil
+    t.datetime "created_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.datetime "updated_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
     t.index ["task_id"], name: "index_time_registers_on_task_id"
   end
 
@@ -47,10 +46,10 @@ ActiveRecord::Schema.define(version: 2022_06_29_150042) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at", precision: 6
-    t.datetime "remember_created_at", precision: 6
-    t.datetime "created_at", precision: 6, default: -> { "CURRENT_TIMESTAMP" }, null: false
-    t.datetime "updated_at", precision: 6, default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.datetime "updated_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
