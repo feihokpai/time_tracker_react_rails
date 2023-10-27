15e6a74ee65640f1880ca81eb66b42b03e6688b2
Fabiano Amorim <fabiano.silva.amorim@gmail.com>
2023-10-27 11:24:10 -0300

    Create a page that request the list of tasks from user and shows the json returned by it.

--- client/src/TasksPage.js
+++ client/src/TasksPage.js
@@ -1,6 +1,39 @@
+import { useState, useEffect } from "react";
+
 function TasksPage(){
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  function getTasks(){
+    const apiUrl = 'http://localhost:3000/task_groups/';
+    fetch(apiUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();        
+      })
+      .then((jsonData) => {
+        setData(jsonData);
+      })
+      .catch((err) => {
+        setError(err);
+      });
+  }
+
+  useEffect(getTasks, []);
+
   return (
-    <div>Hello world</div>
+    <div>
+      <h1>React App</h1>
+      {error && <p>Error: {error.message}</p>}
+      {data && (
+        <div>
+          <h2>Data from Rails Backend:</h2>
+          <pre>{JSON.stringify(data, null, 2)}</pre>
+        </div>
+      )}
+    </div>
   );
 }
 
