b4225e0c701072e0bd42075836f9c9525453a504
Fabiano Amorim <fabiano.silva.amorim@gmail.com>
2023-10-27 11:16:35 -0300

    Remove authentication from backend and set first user in database as default current user.

--- app/controllers/task_groups_controller.rb
+++ app/controllers/task_groups_controller.rb
@@ -1,5 +1,6 @@
 class TaskGroupsController < ApplicationController
-  def index
+  def index    
+    current_user ||= User.first
     task_groups = TaskGroup.where(user: current_user)
     task_groups_as_json = task_groups.map(&:as_json_with_tasks)
     render json: task_groups_as_json
